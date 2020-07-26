d4ecfc3e98df7ddfac43484df1872ed42dc36cb6
Alex Crimi <alexscrimi@gmail.com>
2020-07-26 13:38:01 -0700

    make buttons unclickable before fade in

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('load');
   document.getElementById('map-video').onplay = () => {
-    console.log('play');
     document.getElementById('title-wrapper').style.opacity = 1;
-    document.getElementById('buttons').style.opacity = 1;
+    setTimeout(() => {
+      document.getElementById('buttons').classList.add('visible');
+    }, 9200);
   };
 });
